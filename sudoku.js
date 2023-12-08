a58d29f93b7713d32eb250ca5fa4b7c72236aaf8
SSSamedov <samedov-sultan@mail.ru>
2023-12-08 14:23:52 +0300

    Вариант второй функции

--- sudoku.js
+++ sudoku.js
@@ -1,16 +1,32 @@
 const fs = require('fs')
 const path = require('path')
+const { EOL } = require('os')
 function read() {
-  let readFile = fs.readFileSync(path.join(__dirname, './puzzles.txt'), 'utf-8')
+  let readFile = fs.readFileSync(path.join(__dirname, './puzzles.txt'), 'utf-8').trim().split('')
   return readFile
 }
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+let file1 = read()
+console.log(file1)
+console.log(file1.length);
+
+function solve(str) {
+  const resultArray = [];
+
+  for (let i = 0; i < 15; i++) {
+    const subArray = [];
+    for (let j = 0; j < 9; j++) {
+      const innerSubArray = file1.slice(i * 81 + j * 9, i * 81 + (j + 1) * 9);
+      subArray.push(innerSubArray);
+    }
+    resultArray.push(subArray);
+  }
+
+  return resultArray;
 }
+
+console.log(solve(file1));
+const allArr = solve()
 
 function isSolved() {
   /**
