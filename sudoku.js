9d6016ef683673e74ef3249cc0b4e167b1aa8d2e
natalia <ang_92@list.ru>
2023-12-08 13:31:16 +0300

    наташа ф2

--- sudoku.js
+++ sudoku.js
@@ -1,28 +1,34 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+// const { EOL } = require('os');
+
 function read() {
-  let readFile = fs.readFileSync(path.join(__dirname, './puzzles.txt'), 'utf-8')
-  return readFile
+  const readFile = fs.readFileSync(
+    path.join(__dirname, './puzzles.txt'),
+    'utf-8',
+  );
+  return readFile;
 }
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+const readresult = read();
+
+function solve(arr) {
+  return arr.split('').slice(0, 82);
 }
 
-function isSolved() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
-}
+console.log(solve(readresult));
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
-}
+// function isSolved() {
+//   /**
+//    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//    * Возвращает булевое значение — решено это игровое поле или нет.
+//    */
+// }
+
+// function prettyBoard() {
+//   /**
+//    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+//    * Выводит в консоль/терминал судоку.
+//    * Подумай, как симпатичнее его вывести.
+//    */
+// }
